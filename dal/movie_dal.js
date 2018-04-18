384718bba1560461ed82f16001a03e77e3a8c583
Elliot <elliotn46@gmail.com>
2018-04-17 19:51:07 -0700

    Almost

--- dal/movie_dal.js
+++ dal/movie_dal.js
@@ -34,9 +34,9 @@ exports.insert = function(params, callback) {
 
 
 exports.update = function(params,callback){
-    var query = 'update movie set movie_name = ? , rating_number = ? where movie_id = ?';
+    var query = 'update movie set movie_name = ? , rating_number = ?, date_watched = ? where movie_id = ?';
 
-    var queryData = [params.movie_name, params.movie_rating, params.movie_id];
+    var queryData = [params.movie_name, params.movie_rating, params.date_watched, params.movie_id];
 
     connection.query(query,queryData, function(err,result){
 
